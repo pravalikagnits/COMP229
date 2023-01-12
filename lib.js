8bd34bc347ab92e0fe53a146c45529d4417baead
Pravalika Chada <pravalikareddy.chada@gmail.com>
2023-01-12 12:00:25 -0500

    assignment 1

---
+++ lib.js
@@ -0,0 +1,7 @@
+export function halfOfX(x){
+    return x/2;
+}
+
+export function multiply(x,y){
+    return x*y;
+}
---
+++ main.js
@@ -0,0 +1,3 @@
+import { halfOfX, multiply } from "./lib.js";
+console.log(halfOfX(84));
+console.log(multiply(21, 2));
